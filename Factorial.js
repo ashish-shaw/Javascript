eb47347f6842cabbfa201ff79f000defe5a378b0
ASHISH KUMAR SHAW <91532940+ashish-shaw@users.noreply.github.com>
2022-01-01 23:36:07 +0530

    Add files via upload

---
+++ Factorial.js
@@ -0,0 +1,34 @@
+function factorial(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+function runProgram(input) {
+  // Write code here
+  input = input.trim().split("\n");
+  var n = +input[0];
+  console.log(factorial(n));
+}
+if (process.env.USERNAME === "ashis") {
+  runProgram(`6`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
