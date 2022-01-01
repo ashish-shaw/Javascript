9cfee77ca030f9f0f9358db82e1a2e357704a93f
ASHISH KUMAR SHAW <91532940+ashish-shaw@users.noreply.github.com>
2022-01-01 23:36:21 +0530

    Add files via upload

---
+++ Fibonacci.js
@@ -0,0 +1,34 @@
+function fibonacci(n) {
+  if (n == 0 || n == 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+function runProgram(input) {
+  // Write code here
+  input = input.trim().split("\n");
+  var n = +input[0];
+  console.log(fibonacci(n));
+}
+if (process.env.USERNAME === "ashis") {
+  runProgram(`4`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
